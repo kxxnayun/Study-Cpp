11ff1a14d174bc9eaf050b876693a71cc08b5d20
kimnayun <nayun2179@gmail.com>
2025-10-21 17:16:43 +0900

    section03

--- section03/Account/Account.cpp
+++ section03/Account/Account.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Account {
+private:
+	string name;
+	int id;
+	int balance;
 public:
+	Account(string name, int id, int balance);
+	void deposit(int money);
+	int withdraw(int money);
+	int inquiry();
+	string getOwner();
 };
+
+Account::Account(string name, int id, int balance) {
+	this->name = name;
+	this->id = id;
+	this->balance = balance;
+}
+
+void Account::deposit(int money) {
+	balance += money;
+}
+
+int Account::withdraw(int money) {
+	balance -= money;
+	return balance;
+}
+
+int Account::inquiry() {
+	return balance;
+}
+
+string Account::getOwner() {
+	return name;
+}
+
+int main() {
+	Account a("Kitae", 1, 5000);
+	a.deposit(50000);
+	cout << a.getOwner() << "ÀÇ ÀÜ¾×Àº " << a.inquiry() << endl;
+	int money = a.withdraw(20000);
+	cout << a.getOwner() << "ÀÇ ÀÜ¾×Àº " << a.inquiry() << endl;
+}
